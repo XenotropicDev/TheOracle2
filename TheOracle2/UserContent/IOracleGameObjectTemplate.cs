b57590542f1451e497883b0b3d3883fe0ee21b14
Xeno <eliotd@gmail.com>
2021-11-25 04:00:00 -0800

    feature: draft of templates (I'm really just committing this so that I can access it while I travel)

--- TheOracle2/UserContent/IOracleGameObjectTemplate.cs
+++ TheOracle2/UserContent/IOracleGameObjectTemplate.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TheOracle2.UserContent
@@ -23,7 +24,8 @@ namespace TheOracle2.UserContent
     {
         public string Title { get; set; }
         public string Author { get; set; }
-        public IList<EmbedFieldBuilder> Fields { get; set; } = new List<EmbedFieldBuilder>();
+        public List<FieldInfo> Fields { get; set; } = new();
+        public Dictionary<string, string> Buttons { get; set; } = new Dictionary<string,string>();
 
         public async Task CreateMessage(SocketSlashCommand context)
         {
@@ -38,5 +40,29 @@ namespace TheOracle2.UserContent
 
             await context.RespondAsync(embed: builder.Build(), component: compBuilder.Build());
         }
+    }
+
+    public class FieldInfo : EmbedFieldBuilder
+    {
+        private string title;
+        private string text;
+        private static Regex embededIdRegex = new(@"({\d+})");
+
+        public new string Name { get => title; set => title = value; }
+        public new string Value { get => text; set => text = value; }
+        public new bool IsInline { get; set; } = true;
+
+        private string ParseText(string input)
+        {
+            var ids = embededIdRegex.Matches(input)
+                .Select(m => { 
+                    int.TryParse(m.Groups[1].Value, out int temp); 
+                    return temp; 
+                })
+                .Where(i => i != 0);
+
+            return "Todo";
+        }
+
     }
 }
