27a47011ad55694b9d388b78bf58aae01e0e3ff3
Xeno <eliotd@gmail.com>
2022-01-04 11:40:59 -0800

    fix: formatting

--- TheOracle2/GameObjects/Die.cs
+++ TheOracle2/GameObjects/Die.cs
@@ -1,25 +1,33 @@
 namespace TheOracle2.GameObjects;
-public class Die {
-  private readonly Random random;
-  public readonly int Sides;
-  public int Value { get; set; }
-  private int Roll() {
-    return random.Next(1, Sides + 1);
-  }
-  public void Reroll() {
-    Value = Roll();
-  }
-  /// <summary>
-  ///
-  /// </summary>
-  /// <param name="sides">The number of sides the die has (minimum 2)</param>
-  /// <param name="value">A preset value for the die</param>
-  /// <exception cref="ArgumentOutOfRangeException"></exception>
-  public Die(int sides, int? value) {
-    random = new System.Random();
-    if (sides < 2) { throw new ArgumentOutOfRangeException("Die must have at least 2 sides."); }
-    if (value != null && (value > sides || value < 1)) { throw new ArgumentOutOfRangeException("Face must be null, or a positive integer less than the number of sides on the die."); }
-    Sides = sides;
-    Value = value ?? Roll();
-  }
+
+public class Die
+{
+    private readonly Random random;
+    public readonly int Sides;
+    public int Value { get; set; }
+
+    private int Roll()
+    {
+        return random.Next(1, Sides + 1);
+    }
+
+    public void Reroll()
+    {
+        Value = Roll();
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="sides">The number of sides the die has (minimum 2)</param>
+    /// <param name="value">A preset value for the die</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public Die(int sides, int? value)
+    {
+        random = new System.Random();
+        if (sides < 2) { throw new ArgumentOutOfRangeException("Die must have at least 2 sides."); }
+        if (value != null && (value > sides || value < 1)) { throw new ArgumentOutOfRangeException("Face must be null, or a positive integer less than the number of sides on the die."); }
+        Sides = sides;
+        Value = value ?? Roll();
+    }
 }
