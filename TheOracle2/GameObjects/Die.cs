930d254a9511e0d827fa338b83c7d821c72eb202
rsek <hello@rsek.ca>
2022-01-05 00:22:36 -0800

    additional annotation/errors

--- TheOracle2/GameObjects/Die.cs
+++ TheOracle2/GameObjects/Die.cs
@@ -1,33 +1,36 @@
 namespace TheOracle2.GameObjects;
 
+/// <summary>
+/// Represents a die with an arbitrary number of sides and its current facing.
+/// </summary>
 public class Die
 {
-    private readonly Random random;
-    public readonly int Sides;
-    public int Value { get; set; }
+  private readonly Random random;
+  public int Sides { get; }
+  public int Value { get; set; }
 
-    private int Roll()
-    {
-        return random.Next(1, Sides + 1);
-    }
+  private int Roll()
+  {
+    return random.Next(1, Sides + 1);
+  }
 
-    public void Reroll()
-    {
-        Value = Roll();
-    }
+  public void Reroll()
+  {
+    Value = Roll();
+  }
 
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name="sides">The number of sides the die has (minimum 2)</param>
-    /// <param name="value">A preset value for the die</param>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public Die(int sides, int? value)
-    {
-        random = new System.Random();
-        if (sides < 2) { throw new ArgumentOutOfRangeException("Die must have at least 2 sides."); }
-        if (value != null && (value > sides || value < 1)) { throw new ArgumentOutOfRangeException("Face must be null, or a positive integer less than the number of sides on the die."); }
-        Sides = sides;
-        Value = value ?? Roll();
-    }
+  /// <summary>
+  ///
+  /// </summary>
+  /// <param name="sides">The number of sides the die has (minimum 2)</param>
+  /// <param name="value">A preset value for the die</param>
+  /// <exception cref="ArgumentOutOfRangeException"></exception>
+  public Die(int sides, int? value)
+  {
+    random = new System.Random();
+    if (sides < 2) { throw new ArgumentOutOfRangeException(nameof(sides), "Die must have at least 2 sides."); }
+    if (value != null && (value > sides || value < 1)) { throw new ArgumentOutOfRangeException(nameof(value), "Face must be null, or a positive integer less than the number of sides on the die."); }
+    Sides = sides;
+    Value = value ?? Roll();
+  }
 }
