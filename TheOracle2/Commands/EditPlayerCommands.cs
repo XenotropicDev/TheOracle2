5eb88586f8236fbadc58df4f3ed1c1c3bc847504
Xeno <eliotd@gmail.com>
2022-01-05 16:15:13 -0800

    feature: stored player characters for use in other game mechanics

---
+++ TheOracle2/Commands/EditPlayerCommands.cs
@@ -0,0 +1,35 @@
+﻿using Discord.Interactions;
+using TheOracle2.UserContent;
+
+namespace TheOracle2;
+
+[Group("edit-player", "Edits player stats")]
+public class EditPlayerPaths : InteractionModuleBase
+{
+    public EFContext DbContext { get; }
+
+    public EditPlayerPaths(EFContext dbContext)
+    {
+        DbContext = dbContext;
+    }
+
+    [SlashCommand("add-impact", "Changes the impacts on a character")]
+    public async Task SetImpacts([Autocomplete(typeof(CharacterAutocomplete))] string character, string impact)
+    {
+        if (!int.TryParse(character, out var id)) return;
+        var pc = await DbContext.PlayerCharacters.FindAsync(id);
+
+        pc.Impacts.Add(impact);
+        await DbContext.SaveChangesAsync();
+
+        await RespondAsync($"Impacts will update next time you trigger an interaction on that character card", ephemeral:true);
+
+        //await pc.RedrawCard(Context.Client);
+    }
+
+    [SlashCommand("earned-xp", "Changes the earned xp on a character")]
+    public async Task SetEarnedXp([Autocomplete(typeof(CharacterAutocomplete))] string character,
+                                [Summary(description: "The total amount of Xp this character has earned")][MinValue(0)] int earnedXp)
+    {
+    }
+}
---
+++ TheOracle2/GameObjects/PlayerCharacter.cs
@@ -0,0 +1,103 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheOracle2.GameObjects;
+
+[Index(nameof(MessageId))]
+public class PlayerCharacter
+{
+    private int momentum;
+    private int supply;
+    private int spirit;
+    private int health;
+    private int wits;
+    private int shadow;
+    private int iron;
+    private int heart;
+    private int edge;
+
+    /// <summary>
+    /// This constructor is only for EF/Json
+    /// </summary>
+    public PlayerCharacter()
+    {
+        Health = 5;
+        Spirit = 5;
+        Supply = 5;
+        Momentum = 2;
+        XpGained = 0;
+        XpSpent = 0;
+        Impacts = new List<string>();
+    }
+
+    public PlayerCharacter(ulong ownerId, ulong guildId, ulong messageId, string name, int edge, int heart, int iron, int shadow, int wits)
+    {
+        UserId = ownerId;
+        DiscordGuildId = guildId;
+        MessageId = messageId;
+        Name = name;
+        Edge = edge;
+        Heart = heart;
+        Iron = iron;
+        Shadow = shadow;
+        Wits = wits;
+
+        Health = 5;
+        Spirit = 5;
+        Supply = 5;
+        Momentum = 2;
+        XpGained = 0;
+        XpSpent = 0;
+        Impacts = new List<string>();
+    }
+
+    public PlayerCharacter(SocketInteractionContext interaction, string name, int edge, int heart, int iron, int shadow, int wits)
+    {
+        UserId = interaction.User.Id;
+        DiscordGuildId = interaction.Guild?.Id ?? interaction.User.Id;
+        //MessageId = interaction.Interaction;
+        ChannelId = interaction.Interaction.Channel.Id;
+        Name = name;
+        Edge = edge;
+        Heart = heart;
+        Iron = iron;
+        Shadow = shadow;
+        Wits = wits;
+
+        Health = 5;
+        Spirit = 5;
+        Supply = 5;
+        Momentum = 2;
+        XpGained = 0;
+        XpSpent = 0;
+        Impacts = new List<string>();
+    }
+
+    public int Id { get; set; }
+    public ulong UserId { get; set; }
+    public ulong DiscordGuildId { get; set; }
+    public ulong MessageId { get; set; }
+    public ulong ChannelId { get; set; }
+    public string Name { get; set;  }
+    public int Edge { get => edge; set => edge = (value >= 4) ? 4 : (value <= 1) ? 1 : value; }
+    public int Heart { get => heart; set => heart = (value >= 4) ? 4 : (value <= 1) ? 1 : value; }
+    public int Iron { get => iron; set => iron = (value >= 4) ? 4 : (value <= 1) ? 1 : value; }
+    public int Shadow { get => shadow; set => shadow = (value >= 4) ? 4 : (value <= 1) ? 1 : value; }
+    public int Wits { get => wits; set => wits = (value >= 4) ? 4 : (value <= 1) ? 1 : value; }
+
+    public int Health { get => health; set => health = (value >= 5) ? 5 : (value <= 0) ? 0 : value; }
+    public int Spirit { get => spirit; set => spirit = (value >= 5) ? 5 : (value <= 0) ? 0 : value; }
+    public int Supply { get => supply; set => supply = (value >= 5) ? 5 : (value <= 0) ? 0 : value; }
+    public int Momentum { get => momentum; set => momentum = (value >= 10) ? 10 : (value <= -6) ? -6 : value; }
+    public int XpGained { get; set; }
+    public int XpSpent { get; set; }
+    public string Image { get; set; }
+
+    public IList<string> Impacts { get; set; }
+
+    internal void BurnMomentum()
+    {
+        Momentum = Math.Max(2 - Impacts.Count, 0);
+    }
+}
