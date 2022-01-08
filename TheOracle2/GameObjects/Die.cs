3bd59e6b207d3ac49a9cb3181e71094d33acb39e
rsek <hello@rsek.ca>
2022-01-08 14:02:53 -0800

    adding default value of null

--- TheOracle2/GameObjects/Die.cs
+++ TheOracle2/GameObjects/Die.cs
@@ -22,7 +22,7 @@ public class Die
   /// <param name="sides">The number of sides the die has (minimum 2)</param>
   /// <param name="value">A preset value for the die</param>
   /// <exception cref="ArgumentOutOfRangeException"></exception>
-  public Die(Random random, int sides, int? value)
+  public Die(Random random, int sides, int? value = null)
   {
     if (sides < 2) { throw new ArgumentOutOfRangeException("Die must have at least 2 sides."); }
     if (value != null && (value > sides || value < 1)) { throw new ArgumentOutOfRangeException("Face must be null, or a positive integer less than the number of sides on the die."); }
