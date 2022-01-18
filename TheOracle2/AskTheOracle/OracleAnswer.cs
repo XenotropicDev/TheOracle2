ef7b265334ef73299426e9200f50692b94a4d79f
rsek <hello@rsek.ca>
2022-01-18 02:25:27 -0800

    simplified to AskOption-only version

--- TheOracle2/AskTheOracle/OracleAnswer.cs
+++ TheOracle2/AskTheOracle/OracleAnswer.cs
@@ -1,53 +1,43 @@
 namespace TheOracle2.GameObjects;
 
-public class OracleAnswer : Die, IMatchable
-{
-    public OracleAnswer(Random random, int odds, string question) : base(random, 100)
-    {
-        if (odds is < 1 or > 99) { throw new ArgumentOutOfRangeException(nameof(odds), "Chance must be from 1 to 99, inclusive."); }
+public class OracleAnswer : Die, IMatchable {
+    public OracleAnswer(Random random, AskOption odds, string question) : base(random, 100) {
         Question = question;
         Odds = odds;
     }
-
     public string Question { get; set; }
-    public int Odds { get; }
-    public bool IsYes => Value <= Odds;
-
+    public AskOption Odds { get; }
+    public bool IsYes => Value <= (int)Odds;
     public bool IsMatch => Value == 100 || Value % 11 == 0;
 
-    public override string ToString()
-    {
-        return $"Rolled {Value} vs. {Odds}: **{AnswerString}**";
+    public override string ToString() {
+        return $"Rolled {Value} vs. {(int)Odds}: **{AnswerString}**";
     }
 
     public Color OutcomeColor() => IsYes ? new Color(0x47AEDD) : new Color(0xC50933);
 
     private string MatchMessage => (IsMatch) ? "You rolled a match! Envision an extreme result or twist." : String.Empty;
 
-    private string AnswerString
-    {
-        get
-        {
+    private string AnswerString {
+        get {
             string str = IsYes ? "Yes" : "No";
-            if (IsMatch)
-            {
+            if (IsMatch) {
                 str = $"hell {str}!".ToUpperInvariant();
             }
             return str;
         }
     }
 
-    public static Dictionary<int, string> OddsString => new()
+    public static Dictionary<AskOption, string> OddsString => new()
     {
-        { 10, "Small chance" },
-        { 25, "Unlikely" },
-        { 50, "50/50" },
-        { 75, "Likely" },
-        { 90, "Sure thing" }
+        { AskOption.SmallChance, "Small chance" },
+        { AskOption.Unlikely, "Unlikely" },
+        { AskOption.FiftyFifty, "50/50" },
+        { AskOption.Likely, "Likely" },
+        { AskOption.SureThing, "Sure thing" }
     };
 
-    public EmbedBuilder ToEmbed()
-    {
+    public EmbedBuilder ToEmbed() {
         string authorString = $"Ask the Oracle: {OddsString[Odds]}";
         string footerString = MatchMessage;
         return new EmbedBuilder()
