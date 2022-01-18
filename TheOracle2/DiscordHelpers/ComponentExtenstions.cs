bbdace581e5911d3d72b035eb714b7f79582c9f3
Xeno <eliotd@gmail.com>
2022-01-17 21:37:16 -0800

    refactor: renamed methods, and added some null handling

--- TheOracle2/DiscordHelpers/ComponentExtenstions.cs
+++ TheOracle2/DiscordHelpers/ComponentExtenstions.cs
@@ -4,203 +4,196 @@ namespace TheOracle2;
 
 public static class ContextExtensions
 {
-  public static async Task DeleteOriginalResponseAsync(this SocketSlashCommand ctx)
-  {
-    var msg = await ctx.GetOriginalResponseAsync().ConfigureAwait(false);
-    await msg.DeleteAsync().ConfigureAwait(false);
-  }
+    public static async Task DeleteOriginalResponseAsync(this SocketSlashCommand ctx)
+    {
+        var msg = await ctx.GetOriginalResponseAsync().ConfigureAwait(false);
+        await msg.DeleteAsync().ConfigureAwait(false);
+    }
 }
 
 public static class InteractionExtensions
 {
-
-  public static IMessageComponent GetById(this SocketMessageComponent interaction, string customId)
-  {
-    var components = interaction.Message.Components;
-    ActionRowComponent row = components
-      .FirstOrDefault(r => r.Components.Any(c => c.CustomId == customId));
-    var result = row.Components.FirstOrDefault(c => c.CustomId == customId);
-    return result;
-  }
-  /// <summary>
-  /// Gets the source component of an interaction.
-  /// </summary>
-  public static IMessageComponent GetSrc(this SocketMessageComponent interaction)
-  {
-    string customId = interaction.Data.CustomId;
-    var component = GetById(interaction, customId);
-    return component;
-  }
-  /// <summary>
-  /// Gets the originating menu options of an interaction.
-  /// </summary>
-  public static SelectMenuOption[] GetSrcOptions(this SocketMessageComponent interaction)
-  {
-    var component = GetSrc(interaction);
-    SelectMenuComponent menu = component as SelectMenuComponent;
-    var values = interaction.Data.Values as string[];
-    var results = menu.Options.Where(option => values.Contains<string>(option.Value));
-    return results as SelectMenuOption[];
-  }
-  /// <summary>
-  /// Gets the first of the orginating menu options of an interaction
-  /// </summary>
-  public static SelectMenuOption GetSrcOption(this SocketMessageComponent interaction)
-  {
-    var value = interaction.Data.Values.FirstOrDefault();
-    return GetSrcOption(interaction, value);
-  }
-  /// <summary>
-  /// Gets the first option of the originating menu with a matching value.
-  /// </summary>
-  public static SelectMenuOption GetSrcOption(this SocketMessageComponent interaction, string value)
-  {
-    var component = GetSrc(interaction);
-    SelectMenuComponent menu = component as SelectMenuComponent;
-    var result = menu.Options.FirstOrDefault(option => value == option.Value);
-    return result;
-  }
-  /// <summary>
-  /// Gets the label of the first originating menu option.
-  /// </summary>
-  public static string GetSrcOptionLabel(this SocketMessageComponent interaction)
-  {
-    var option = GetSrcOption(interaction);
-    return option.Label;
-  }
-  /// <summary>
-  /// Gets the labels of the originating menu options.
-  /// </summary>
-  public static string[] GetSrcOptionLabels(this SocketMessageComponent interaction)
-  {
-    var options = GetSrcOptions(interaction);
-    return options.Select(option => option.Label) as string[];
-  }
-  /// <summary>
-  /// Gets the emoji of the first originating menu option.
-  /// </summary>
-  public static IEmote GetSrcOptionEmoji(this SocketMessageComponent interaction)
-  {
-    var option = GetSrcOption(interaction);
-    return option.Emote;
-  }
-  /// <summary>
-  /// Gets the emojis of the originating menu options.
-  /// </summary>
-  public static IEmote[] GetSrcOptionEmojis(this SocketMessageComponent interaction)
-  {
-    var options = GetSrcOptions(interaction);
-    return options.Select(option => option.Emote) as IEmote[];
-  }
-
-  /// <summary>
-  /// Gets the description of the first originating menu option.
-  /// </summary>
-  public static string GetSrcOptionDescription(this SocketMessageComponent interaction)
-  {
-    var option = GetSrcOption(interaction);
-    return option.Description;
-  }
-  /// <summary>
-  /// Gets the emojis of the originating menu options.
-  /// </summary>
-  public static string[] GetSrcOptionDescriptions(this SocketMessageComponent interaction)
-  {
-    var options = GetSrcOptions(interaction);
-    return options.Select(option => option.Description) as string[];
-  }
-}
-
-
-
-public static class EmbedExtensions
-{
-  /// <summary>
-  /// Changes a numeric field on an embed
-  /// </summary>
-  /// <param name="embed">the embed builder to change</param>
-  /// <param name="fieldName">the name of the field to look for</param>
-  /// <param name="change">the amount to change by (use negative numbers to decrease)</param>
-  /// <param name="min">the lowest the value can be</param>
-  /// <param name="max">the highest the value can be</param>
-  /// <returns>The embed builder provided</returns>
-  public static EmbedBuilder ChangeNumericField(this EmbedBuilder embed, string fieldName, int change, int min, int max)
-  {
-    int index = embed.Fields.FindIndex(f => f.Name == fieldName);
-    if (int.TryParse(embed.Fields[index].Value.ToString(), out var value))
+    /// <summary>
+    /// Gets the component with the matching ID, if any.
+    /// </summary>
+    /// <param name="interaction">The interaction to get the component out of</param>
+    /// <param name="customId">The ID to search for</param>
+    /// <returns></returns>
+    public static IMessageComponent GetComponentById(this SocketMessageComponent interaction, string customId)
     {
-      value += change;
-      if (value < min) value = min;
-      if (value > max) value = max;
-      embed.Fields[index].Value = value;
+        var components = interaction.Message.Components;
+        ActionRowComponent row = components
+          .FirstOrDefault(r => r.Components.Any(c => c.CustomId == customId));
+        var result = row?.Components.FirstOrDefault(c => c.CustomId == customId);
+        return result;
     }
-    return embed;
-  }
+
+    /// <summary>
+    /// Gets the triggering component of an interaction.
+    /// </summary>
+    public static IMessageComponent GetTriggeringComponent(this SocketMessageComponent interaction)
+    {
+        string customId = interaction.Data.CustomId;
+        var component = GetComponentById(interaction, customId);
+        return component;
+    }
+
+    /// <summary>
+    /// Gets the originating menu options of an interaction.
+    /// </summary>
+    /// <param name="interaction">The interaction to get the select menus out of</param>
+    /// <returns></returns>
+    public static SelectMenuOption[] GetTriggeringSelectMenuItems(this SocketMessageComponent interaction)
+    {
+        var component = GetTriggeringComponent(interaction);
+        SelectMenuComponent menu = component as SelectMenuComponent;
+        var values = interaction.Data.Values;
+        var results = menu?.Options.OfType<SelectMenuOption>().Where(option => values.Contains(option.Value));
+        return results.ToArray();
+    }
+
+    /// <summary>
+    /// Gets the first of the originating menu options of an interaction
+    /// </summary>
+    public static SelectMenuOption GetFirstSelectMenu(this SocketMessageComponent interaction)
+    {
+        var value = interaction.Data.Values.FirstOrDefault();
+        return GetSelectItemWithId(interaction, value);
+    }
+
+    /// <summary>
+    /// Gets the first select menu option out of an with a matching value.
+    /// </summary>
+    public static SelectMenuOption GetSelectItemWithId(this SocketMessageComponent interaction, string value)
+    {
+        if (value == null) return null;
+
+        var component = GetTriggeringComponent(interaction);
+        SelectMenuComponent menu = component as SelectMenuComponent;
+        var result = menu?.Options.FirstOrDefault(option => value == option.Value);
+        return result;
+    }
+
+    // Todo, I'm not sure if this adds enough to justify the extension method.
+    /// <summary>
+    /// Gets the label of the first originating menu option.
+    /// </summary>
+    public static string GetFirstSelectMenuLabel(this SocketMessageComponent interaction)
+    {
+        var option = GetFirstSelectMenu(interaction);
+        return option?.Label;
+    }
+
+    /// <summary>
+    /// Gets the labels of the originating menu options.
+    /// </summary>
+    public static string[] GetSrcOptionLabels(this SocketMessageComponent interaction)
+    {
+        var options = GetTriggeringSelectMenuItems(interaction);
+        return options?.Select(option => option.Label).ToArray();
+    }
+
+    /// <summary>
+    /// Gets the emoji of the first originating menu option.
+    /// </summary>
+    public static IEmote GetFirstSelectEmote(this SocketMessageComponent interaction)
+    {
+        var option = GetFirstSelectMenu(interaction);
+        return option?.Emote;
+    }
+
+    /// <summary>
+    /// Gets the emojis of the originating menu options.
+    /// </summary>
+    public static IEmote[] GetTriggeringEmote(this SocketMessageComponent interaction)
+    {
+        var options = GetTriggeringSelectMenuItems(interaction);
+        return options?.Select(option => option.Emote).ToArray();
+    }
+
+    // Todo, I'm not sure if this adds enough to justify the extension method.
+    /// <summary>
+    /// Gets the description of the first originating menu option.
+    /// </summary>
+    public static string GetSrcOptionDescription(this SocketMessageComponent interaction)
+    {
+        var option = GetFirstSelectMenu(interaction);
+        return option.Description;
+    }
+
+    /// <summary>
+    /// Gets the emojis of the originating menu options.
+    /// </summary>
+    public static string[] GetSrcOptionDescriptions(this SocketMessageComponent interaction)
+    {
+        var options = GetTriggeringSelectMenuItems(interaction);
+        return options.Select(option => option.Description).ToArray();
+    }
 }
 
 public static class ComponentExtenstions
 {
-  public static ComponentBuilder RemoveComponentById(this ComponentBuilder builder, string id)
-  {
-    var row = builder.ActionRows.FindIndex(r => r.Components.Any(c => c.CustomId == id));
-    var item = builder.ActionRows[row].Components.Find(c => c.CustomId == id);
-    if (item != null)
+    public static ComponentBuilder RemoveComponentById(this ComponentBuilder builder, string id)
     {
-      builder.ActionRows[row].Components.Remove(item);
-      if (builder.ActionRows[row].Components.Count == 0)
-      {
-        builder.ActionRows.Remove(builder.ActionRows[row]);
-      }
-    }
-
-    return builder;
-  }
-
-  public static ComponentBuilder ReplaceComponentById(this ComponentBuilder builder, string id, IMessageComponent replacement)
-  {
-    var rows = builder.ActionRows.Where(r => r.Components.Any(c => c.CustomId == id));
-    foreach (var row in rows)
-    {
-      int index = row.Components.FindIndex(c => c.CustomId == id);
-      if (index != -1) row.Components[index] = replacement;
-    }
-
-    return builder;
-  }
-
-  public static ComponentBuilder TryAdd(this ComponentBuilder builder, IMessageComponent component, int row)
-  {
-    if (builder.ActionRows.Any(r => r.Components.Any(c => c.CustomId == component.CustomId))) return builder;
-
-    if (builder.ActionRows.Count - 1 >= row)
-    {
-      builder.ActionRows[row].Components.Add(component);
-      return builder;
-    }
-
-    var currentRow = new ActionRowBuilder().AddComponent(component);
-
-    builder.ActionRows.Add(currentRow);
-
-    return builder;
-  }
-
-  public static ComponentBuilder MarkSelectionByOptionId(this ComponentBuilder component, string optionValue)
-  {
-    foreach (var row in component.ActionRows)
-    {
-      var selectBuilder = (row.Components.FirstOrDefault() as SelectMenuComponent)?.ToBuilder();
-      if (selectBuilder != null)
-      {
-        var option = selectBuilder.Options.FirstOrDefault(o => o.Value == optionValue);
-        if (option != null)
+        var row = builder.ActionRows.FindIndex(r => r.Components.Any(c => c.CustomId == id));
+        var item = builder.ActionRows[row].Components.Find(c => c.CustomId == id);
+        if (item != null)
         {
-          option.WithDefault(true);
-          row.WithComponents(new List<IMessageComponent> { selectBuilder.Build() });
+            builder.ActionRows[row].Components.Remove(item);
+            if (builder.ActionRows[row].Components.Count == 0)
+            {
+                builder.ActionRows.Remove(builder.ActionRows[row]);
+            }
         }
-      }
+
+        return builder;
     }
 
-    return component;
-  }
+    public static ComponentBuilder ReplaceComponentById(this ComponentBuilder builder, string id, IMessageComponent replacement)
+    {
+        var rows = builder.ActionRows.Where(r => r.Components.Any(c => c.CustomId == id));
+        foreach (var row in rows)
+        {
+            int index = row.Components.FindIndex(c => c.CustomId == id);
+            if (index != -1) row.Components[index] = replacement;
+        }
+
+        return builder;
+    }
+
+    public static ComponentBuilder TryAdd(this ComponentBuilder builder, IMessageComponent component, int row)
+    {
+        if (builder.ActionRows.Any(r => r.Components.Any(c => c.CustomId == component.CustomId))) return builder;
+
+        if (builder.ActionRows.Count - 1 >= row)
+        {
+            builder.ActionRows[row].Components.Add(component);
+            return builder;
+        }
+
+        var currentRow = new ActionRowBuilder().AddComponent(component);
+
+        builder.ActionRows.Add(currentRow);
+
+        return builder;
+    }
+
+    public static ComponentBuilder MarkSelectionByOptionId(this ComponentBuilder component, string optionValue)
+    {
+        foreach (var row in component.ActionRows)
+        {
+            var selectBuilder = (row.Components.FirstOrDefault() as SelectMenuComponent)?.ToBuilder();
+            if (selectBuilder != null)
+            {
+                var option = selectBuilder.Options.FirstOrDefault(o => o.Value == optionValue);
+                if (option != null)
+                {
+                    option.WithDefault(true);
+                    row.WithComponents(new List<IMessageComponent> { selectBuilder.Build() });
+                }
+            }
+        }
+
+        return component;
+    }
 }
