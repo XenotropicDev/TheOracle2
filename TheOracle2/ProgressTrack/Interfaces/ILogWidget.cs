25f08df67a3035e4a87c8e47f4108148a228ee11
rsek <hello@rsek.ca>
2022-01-18 02:32:52 -0800

    methods for parsing whether alerts should be sent

--- TheOracle2/ProgressTrack/Interfaces/ILogWidget.cs
+++ TheOracle2/ProgressTrack/Interfaces/ILogWidget.cs
@@ -1,35 +1,43 @@
+using TheOracle2.DiscordHelpers;
 namespace TheOracle2.GameObjects;
 
 /// <summary>
 /// Interface for game widgets that can increment/decrement and might want to report such events to a log or embed.
 /// </summary>
-public interface ILogWidget : IWidget
-{
+public interface ILogWidget : IWidget {
     public bool AlertOnIncrement { get; }
     public bool AlertOnDecrement { get; }
     public bool LogOnIncrement { get; }
     public bool LogOnDecrement { get; }
-
     /// <summary>
     /// A message string. Sent to the log, and used as the alert embed title.
     /// </summary>
     public string LogMessage { get; }
-
     public string AlertFooter { get; }
-
     public EmbedBuilder AlertEmbed();
-
+    public static bool ParseAlertStatus(ButtonComponent button) {
+        return button.CustomId switch {
+            "alerts-toggle:True" => true,
+            "alerts-toggle:False" => false,
+            _ => throw new Exception($"Unable to parse alert status from customId: {button.CustomId}")
+        };
+    }
+    public static bool ParseAlertStatus(IEnumerable<ActionRowComponent> actionRows) {
+        return actionRows.GetComponentById("alerts-toggle:True") != null;
+    }
+    public static bool ParseAlertStatus(IMessage message) {
+        var components = message.Components as IEnumerable<ActionRowComponent>;
+        return ParseAlertStatus(components);
+    }
     /// <summary>
     /// Template/stub for use in building alert embeds.
     /// </summary>
-    public static EmbedBuilder AlertStub(ILogWidget widget)
-    {
+    public static EmbedBuilder AlertStub(ILogWidget widget) {
         EmbedBuilder embed = new EmbedBuilder()
-          .WithAuthor($"{widget.EmbedCategory}: {widget.Title}")
-          .WithTitle(widget.LogMessage)
-          ;
-        if (!string.IsNullOrEmpty(widget.AlertFooter))
-        {
+            .WithAuthor($"{widget.EmbedCategory}: {widget.Title}")
+            .WithTitle(widget.LogMessage)
+            ;
+        if (!string.IsNullOrEmpty(widget.AlertFooter)) {
             embed.WithFooter(widget.AlertFooter);
         }
         return embed;
