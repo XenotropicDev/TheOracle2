5b3eacf723eb5d27e9881342d74ac5ee19f69f52
rsek <hello@rsek.ca>
2022-01-22 17:04:18 -0800

    minor description changes

--- TheOracle2/Commands/AskTheOracleCommand.cs
+++ TheOracle2/Commands/AskTheOracleCommand.cs
@@ -3,26 +3,29 @@ using TheOracle2.GameObjects;
 
 namespace TheOracle2;
 
-public class OracleAskPaths : InteractionModuleBase {
+public class OracleAskPaths : InteractionModuleBase
+{
     private readonly Random random;
 
-    public OracleAskPaths(Random random) {
+    public OracleAskPaths(Random random)
+    {
         this.random = random;
     }
 
-    [SlashCommand("ask", "Ask the oracle based on the predefined likelihoods")]
+    [SlashCommand("ask", "Ask the Oracle a yes/no question (p. 225). To roll on a specific oracle table, use /oracle.")]
     public async Task AskTheOracle(
-        [Summary(description: "The odds of receiving a 'yes'.")]
+
+        [Summary(description: "The question to ask the oracle.")]
+        string question,
+        [Summary(description: "The odds of receiving a 'yes' answer.")]
         [Choice("Sure thing (10 or less)", 10),
         Choice("Likely (25 or less)", 25),
         Choice("50/50 (50 or less)", 50),
         Choice("Unlikely (75 or less)", 75),
         Choice("Small chance (90 or less)", 90)]
-        int odds,
-        [Summary(description: "The question to ask the oracle.")]
-        string question
-    ) {
-
+        int odds
+    )
+    {
         /// TODO: once discord display string attributes are available for enums, this can use the AskOption enum directly
         await RespondAsync(embed: new OracleAnswer(random, (AskOption)odds, question).ToEmbed().Build()).ConfigureAwait(false);
     }
